fc13cef8cd3153a6b54c08ec8b23a595ae5b1642
edledesma <100038680+edledesma@users.noreply.github.com>
2023-09-30 15:42:31 -0300

    Img2Text new version

--- Projects/PyCarBase/main.py
+++ Projects/PyCarBase/main.py
@@ -2,7 +2,7 @@ from src.Services.menuServices import *
 
 root = tk.Tk()
 root.title("CarBase")
-root.iconbitmap('src\Media\icon.ico')
+root.iconbitmap('src\Media\hatch.ico')
 root.geometry("600x400")
 menuOptions(root)
 
