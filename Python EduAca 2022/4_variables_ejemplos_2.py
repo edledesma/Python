4ed8d0989a9b751f80c4005ae1f37ccefcbdd12e
edledesma <100038680+edledesma@users.noreply.github.com>
2023-10-03 20:42:31 -0300

    Updated repository

--- Python EduAca 2022/4_variables_ejemplos_2.py
+++
@@ -1,7 +0,0 @@
-# Variables para nombre, apellido, nacionalidad y edad
-nombre = "Joaquin"
-apellido = "Romero"
-nacionalidad = "Argentino"
-edad = 41
-
-print(f"Hola {nombre}, tu apellido es {apellido}. Sos {nacionalidad} con {edad} años de edad ")
